1b0fda58f21d088de0a630e38f36ded5c52555da
kse201 <kse.201@gmail.com>
2017-03-11 09:55:49 +0900

    feature: Implement test code
    - add serverspec
    - add box centos/7 and ubuntu/trusty

---
+++ tests/spec/ansible_dotfiles_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+pkgs = case os[:family]
+       when 'redhat'
+         %w(vim-enhanced git fish tmux)
+       when os[:family]
+         %w(vim git fish tmux)
+       end
+
+files = %w(.vimrc .gitconfig .config/fish .tmux.conf)
+
+describe os[:family] do
+  pkgs.each do |pkg|
+    context package(pkg) do
+      it { should be_installed }
+    end
+  end
+end
+
+files.each do |cfg|
+  user = case Specinfra::Configuration.defaults[:backend]
+         when :exec
+           `whoami`.chomp
+         when :ssh
+           Specinfra::Configuration.defaults[:ssh_options][:user]
+         end
+
+  describe file("/home/#{user}/#{cfg}") do
+    it { should be_exist }
+  end
+end
