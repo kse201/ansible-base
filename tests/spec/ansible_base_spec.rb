f43f09798fe303a61c6814db627c4b7ad871c829
kse201 <kse.201@gmail.com>
2017-03-29 21:13:23 +0900

    feature: support archlinux only

--- tests/spec/ansible_base_spec.rb
+++ tests/spec/ansible_base_spec.rb
@@ -22,12 +22,6 @@ user = case Specinfra::Configuration.defaults[:backend]
          Specinfra::Configuration.defaults[:ssh_options][:user]
        end
 
-pips = %w( neovim
-           vim-vint
-           ansible-lint )
-
-# gems = %w( rubocop tmuxinator )
-
 describe os[:family] do
   pkgs.each do |pkg|
     context package(pkg) do
@@ -44,34 +38,7 @@ files.each do |cfg|
 end
 
 describe 'Ruby environment' do
-  describe file('/usr/local/rbenv/bin/rbenv') do
-    it { should be_file }
-    it { should be_executable }
-  end
-
-  describe command('/usr/local/rbenv/shims/ruby --version') do
-    its(:stdout) { should match '2.3.3' }
-  end
-
-  # gems.each do |gem|
-  # describe package(gem) do
-  # it { should be_installed.by('gem') }
-  # end
-  # end
 end
 
 describe 'Python environment' do
-  describe file("/home/#{user}/.pyenv/bin/pyenv") do
-    it { should exist }
-  end
-
-  describe command("/home/#{user}/.pyenv/shims/python --version") do
-    its(:stdout) { should match '3.5.0' }
-  end
-
-  pips.each do |pip|
-    describe command("/home/#{user}/.pyenv/shims/pip list") do
-      its(:stdout) { should match pip }
-    end
-  end
 end
